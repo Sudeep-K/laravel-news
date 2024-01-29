2f8e6c8d9258c8a2a3ce6645986f577a44cdf9f4
Sudeep-K <pong32btl@gmail.com>
2024-01-29 15:22:13 +0545

    migration for news, tags and news_tags created

---
+++ app/Models/News.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class News extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'news';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'banner_image',
+        'category_id'
+    ];
+}
