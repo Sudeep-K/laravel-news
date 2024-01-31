8f1a7772c760a4ffb2f4632ccff1a0a0375ce4d0
prabin-bhusal <prabinbhusal.np@gmail.com>
2024-01-31 16:01:41 +0545

    news dashboard ui changes

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -1,77 +1,96 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Create News Article') }}
+        </h2>
+    </x-slot>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div>
+                        <div class="">
 
-<body>
+                            <form method="post" action="{{ route('news.store') }}" enctype="multipart/form-data"
+                                class="max-w-md mx-auto">
+                                @csrf
+                                @method('post')
 
-    <h1>Create News Page</h1>
 
-    <form method="post" action="{{ route('news.store') }}" enctype="multipart/form-data">
-        @csrf
-        @method('post')
 
-        <div class="form-group">
-            <label for="title" name="title" class="form-label">Title</label>
-            <input type="text" name="title" placeholder="Enter title" class="form-control"
-                value="{{ old('title') }}" />
-            <span>
-                @error('title')
-                    {{ $message }}
-                @enderror
-            </span>
+
+                                <div class="mb-5">
+                                    <label for="title" name="title" class="form-label"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                                    <input type="text" name="title"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Enter title" class="form-control" value="{{ old('title') }}" />
+                                    <span>
+                                        @error('title')
+                                            {{ $message }}
+                                        @enderror
+                                    </span>
+                                </div>
+                                <div class="mb-5">
+                                    <label for="content" name="content"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
+                                    <textarea rows="8" type="text" name="content" placeholder="Enter content"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">{{ old('content') }}</textarea>
+                                    <span>
+                                        @error('content')
+                                            {{ $message }}
+                                        @enderror
+                                    </span>
+                                </div>
+                                <div class="mb-5">
+                                    <label for="image" name="image"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Banner
+                                        Image</label>
+                                    <input type="file" name="image" placeholder="Enter title"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                    <span>
+                                        @error('image')
+                                            {{ $message }}
+                                        @enderror
+                                    </span>
+                                </div>
+                                <div class="mb-5">
+                                    <label for="category_id" name="category_id"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Categories</label>
+                                    <select name="category_id"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                                        @foreach ($categories as $category)
+                                            <option value={{ $category->id }}>{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <span>
+                                        @error('category_id')
+                                            {{ $message }}
+                                        @enderror
+                                    </span>
+                                </div>
+                                <br>
+
+                                <div class="mb-5">
+                                    <label for="tags" name="tags"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tags</label>
+                                    <input type="text" name="tags"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                </div>
+
+                                <br>
+
+
+
+                                <button type="submit"
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-            <label for="content" name="content">Content</label>
-            <textarea type="text" name="content" placeholder="Enter content">{{ old('content') }}</textarea>
-            <span>
-                @error('content')
-                    {{ $message }}
-                @enderror
-            </span>
-        </div>
-        <div>
-            <label for="image" name="image">Banner Image</label>
-            <input type="file" name="image" placeholder="Enter title" />
-            <span>
-                @error('image')
-                    {{ $message }}
-                @enderror
-            </span>
-        </div>
-        <div>
-            <label for="category_id" name="category_id">Categories</label>
-            <select name="category_id">
-
-                @foreach ($categories as $category)
-                    <option value={{ $category->id }}>{{ $category->name }}</option>
-                @endforeach
-            </select>
-            <span>
-                @error('category_id')
-                    {{ $message }}
-                @enderror
-            </span>
-        </div>
-        <br>
-
-        <div>
-            <label for="tags" name="tags">Tags</label>
-            <input type="text" name="tags" />
-        </div>
-
-        <br>
-
-
-
-        <button type="submit">Submit</button>
-    </form>
-
-</body>
-
-</html>
+    </div>
+</x-app-layout>
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,44 +1,110 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-
-</head>
-
-<body>
-    <h1>News Home Page</h1>
-    <a href="{{ route('news.create') }}">Create New News</a>
-
-    <div>
-        @if (Session::has('message'))
-            <div>
-                <p>Informational message</p>
-                <p>{{ Session::get('message') }}</p>
-            </div>
-        @endif
-    </div>
-    @foreach ($news as $singleNews)
-        <h2>
-            <a href="{{ route('news.show', $singleNews->id) }}">{{ $singleNews->title }}</a>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Laravel News') }}
         </h2>
-        <div>
-            <img src="{{ asset($singleNews->banner_image) }}" alt=" " style="height:200px;" />
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div>
+                @if (Session::has('message'))
+                    <div class="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+                        role="alert">
+                        <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                        </svg>
+                        <span class="sr-only">Info</span>
+                        <div>
+                            <span class="font-medium">Success alert!</span> {{ Session::get('message') }}
+                        </div>
+                    </div>
+                @endif
+            </div>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    {{-- <a href="{{ route('news.create') }}">Create New News</a> --}}
+                    <div class="flex justify-between mx-2 my-2">
+                        <a href="{{ route('news.create') }}" type="button"
+                            class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Create
+                            New Blog</a>
+                        {{-- <button type="button"
+                            class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Create
+                            New Category</button> --}}
+
+                        <button type="button"
+                            class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Create
+                            New Category</button>
+                    </div>
+
+
+                    <div>
+                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead
+                                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3">
+                                            ID
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            NEWS TITLE
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Category
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            IMAGE
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            <span class="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    @foreach ($news as $singleNews)
+                                        <tr
+                                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <th scope="row"
+                                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {{ $loop->iteration }}
+                                            </th>
+                                            <td class="px-6 py-4">
+                                                {{ $singleNews->title }}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                {{ $singleNews->category->name }}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <a href="{{ asset($singleNews->banner_image) }}" target="_blank">
+                                                    <img src="{{ asset($singleNews->banner_image) }}"
+                                                        style="height:100px;" alt="thumbnail" />
+                                                </a>
+                                            </td>
+                                            <td class="px-6 py-4 text-right flex gap-x-3">
+                                                <a href="{{ route('news.edit', $singleNews->id) }}"
+                                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                                <form action="{{ route('news.destroy', ['news' => $singleNews->id]) }}"
+                                                    method="post">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <input style="cursor: pointer" type="submit" value="Delete"
+                                                        class="font-medium text-red-600 dark:text-red-500 hover:underline" />
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <p>{{ $singleNews->content }}</p>
-        <a href="{{ route('news.edit', $singleNews->id) }}">Edit</a>
-
-        <form action="{{ route('news.destroy', ['news' => $singleNews->id]) }}" method="post">
-            @csrf
-            @method('delete')
-            <input type="submit" value="Delete" />
-        </form>
-        <hr>
-    @endforeach
-</body>
-
-</html>
+    </div>
+</x-app-layout>
